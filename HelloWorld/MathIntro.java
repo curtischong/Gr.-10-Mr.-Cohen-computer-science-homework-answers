c6fd20e9ec9f6dee759a5fc1bf946c0c6a724a86
splacorn <curtischong5@gmail.com>
2016-02-04 13:41:09 -0500

    learned some math

---
+++ HelloWorld/MathIntro.java
@@ -0,0 +1,16 @@
+public class MathIntro{
+    public static void main (String[] args){
+    //example
+    double area;
+    double radius;
+    
+    //set radius
+    radius = 5;
+    //Calculate area
+    
+    area = radius*radius * 3.14159;
+    //output to user
+    System.out.println("The area of a circle w/ radius of "+radius+"is "+area);
+    }
+}
+
