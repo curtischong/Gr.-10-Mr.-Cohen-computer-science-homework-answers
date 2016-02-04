644fad1a6c5c8fd74c6c2a4adb9c0e4607f439d6
splacorn <curtischong5@gmail.com>
2016-02-04 14:07:02 -0500

    optimised code for math

--- HelloWorld/MathIntro.java
+++ HelloWorld/MathIntro.java
@@ -8,6 +8,7 @@ public class MathIntro{
     radius = 5;
     //Calculate area
     
+    
     area = radius*radius * 3.14159;
     //output to user
     System.out.println("The area of a circle w/ radius of "+radius+"is "+area);
