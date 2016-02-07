1ef41475025d8c50635acfe9ae141a5ba1de4822
splacorn <curtischong5@gmail.com>
2016-02-07 14:28:21 -0500

    a test to discover how the class name changes with the filename in java

---
+++ ticket purchase/s.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class s{
+    
+    private static Scanner get = new Scanner(System.in);
+    
+    public static void main(String[] args){
+        System.out.println("hi");
+    }
+}
