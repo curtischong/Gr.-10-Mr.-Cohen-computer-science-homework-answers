5f2db83b20e184fd416cb4e427b624a45e50c249
splacorn <curtischong5@gmail.com>
2016-02-07 14:25:55 -0500

    this program aks for you to type something and returns what you'd typed

---
+++ ticket purchase/hey.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class hey{
+    
+    private static Scanner get = new Scanner(System.in);
+    
+    public static void main(String[] args){
+        System.out.println("filler");
+        String foo = get.nextLine();
+        String doo = get.nextLine();
+        System.out.println(foo+doo);
+    }
+}
