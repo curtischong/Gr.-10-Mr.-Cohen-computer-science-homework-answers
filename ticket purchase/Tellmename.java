d1faf4916d02f0b60d42b96f0eb10e4bf7660196
splacorn <curtischong5@gmail.com>
2016-02-07 14:26:34 -0500

    this program asks for your name and returns it back to you

---
+++ ticket purchase/Tellmename.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Tellmename{
+    private static Scanner get = new Scanner(System.in);
+    public static void main (String[] args){
+        System.out.println("what's your name");
+        String name = get.nextLine();
+        System.out.println("asdasdsafd");
+        get.nextLine();
+        String hey = get.nextLine();
+        System.out.println("Your name is "+name+hey);
+    }
+}
