f7916953286f036168830cc5b23d304ae9cb22f6
splacorn <curtischong5@gmail.com>
2016-02-07 14:25:34 -0500

    more practicing

---
+++ ticket purchase/flop.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class flop{
+    private static Scanner get = new Scanner(System.in);
+    public static void main (String[] args){
+        System.out.println("hey");
+        String hey = get.nextLine();
+        System.out.println("hai");
+        double hai = get.nextDouble();
+        System.out.println("<3");
+        get.nextLine();
+        String heart = get.nextLine();
+        System.out.println(hey+hai+heart);
+    }
+    
+}
