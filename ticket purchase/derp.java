53830e6201b4a88563327a65a95139522d65ec79
splacorn <curtischong5@gmail.com>
2016-02-07 14:24:48 -0500

    practicing open syntax

---
+++ ticket purchase/derp.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class derp{
+    private static Scanner get = new Scanner(System.in);
+    public static void main(String[] args){
+        System.out.println("Gimmie some cash");
+        int hi = get.nextInt();
+        System.out.println("what's ur flaasdb");
+        get.nextLine();
+        String fat = get.nextLine();
+        System.out.println("you have "+hi+"cash &"+fat+"sad");
+        System.out.println(hi);
+    }
+}
