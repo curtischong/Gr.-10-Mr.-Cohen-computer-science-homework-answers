57bcbe426ba689ef70351b2cfbef14286fb0b869
splacorn <curtischong5@gmail.com>
2016-02-09 17:04:14 -0500

    a program that calculates your age in percent

---
+++ casting/age.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class age{
+    private static Scanner get = new Scanner(System.in);
+    public static void main(String[] args){
+    System.out.print("Please enter your age: ");
+    int age = get.nextInt();
+    System.out.print("Number of days since your birthday: ");
+    double percent = get.nextDouble();
+    percent = percent/365;
+    double newage = (double)age+percent;
+    System.out.println("You are " + newage+ " years old");
+    System.out.println("Rounded to the closest year, you are "+(int)Math.round(newage));
+    }
+}
