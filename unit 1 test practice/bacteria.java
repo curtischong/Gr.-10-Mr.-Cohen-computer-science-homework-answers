f38d4f89119eb6605c7f9e5ef4aac9ff3f477d97
splacorn <curtischong5@gmail.com>
2016-02-11 18:29:04 -0500

    started bacteria question

---
+++ unit 1 test practice/bacteria.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class bacteria{
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args){
+        double bacteria = 1230;
+        System.out.println("What are the number of hours?");
+        double hours = scanner.nextInt();
+        bacteria = Math.pow(bacteria,hours);
+        System.out.println ("Number of bacteria: " + bacteria + " bacterias");
+    }
+}
