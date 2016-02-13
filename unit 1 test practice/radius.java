bbf5ea443b257e06588c96d8c9bc4466ec30e822
splacorn <curtischong5@gmail.com>
2016-02-13 12:13:25 -0500

    updated radius and height program

--- unit 1 test practice/radius.java
+++ unit 1 test practice/radius.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class radius{
-    private static Scanner scanner = new Scanner();
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args){
         System.out.println("enter a radius");
         double radius = scanner.nextDouble();
