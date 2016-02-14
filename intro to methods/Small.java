3caba34598068dab571ff4e4f3b02c883055909f
splacorn <curtischong5@gmail.com>
2016-02-14 13:24:06 -0500

    intro to methods program

---
+++ intro to methods/Small.java
@@ -0,0 +1,9 @@
+public class Small{
+public static void main(String[] args){
+    System.out.println(calculatePerimeter(4,7));
+    
+}
+public static int calculatePerimeter (int side1, int side2){
+    return (side1*2) + (side2*2);
+}
+}
